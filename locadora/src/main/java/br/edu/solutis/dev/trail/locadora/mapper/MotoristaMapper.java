9022704ee1a583f19b8d5df36c020feb649288d3
DanielNaiff <danielnaiff344@gmail.com>
2024-08-30 20:31:06 -0300

    feat: motoristaMapper

---
+++ locadora/src/main/java/br/edu/solutis/dev/trail/locadora/mapper/MotoristaMapper.java
@@ -0,0 +1,21 @@
+package br.edu.solutis.dev.trail.locadora.mapper;
+
+import br.edu.solutis.dev.trail.locadora.model.dto.pessoa.MotoristaDTO;
+import br.edu.solutis.dev.trail.locadora.model.entity.pessoa.MotoristaEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface MotoristaMapper {
+
+    MotoristaMapper INSTANCE = Mappers.getMapper(MotoristaMapper.class);
+
+    // Mapeia Motorista para MotoristaDTO
+    @Mapping(source = "aniversario", target = "aniversario")
+    MotoristaDTO toDTO(MotoristaEntity motorista);
+
+    // Mapeia MotoristaDTO para Motorista
+    @Mapping(source = "aniversario", target = "aniversario")
+    MotoristaEntity toEntity(MotoristaDTO motoristaDTO);
+}
