060b86ea27366fcc7ba26495819df2afa418900b
Hugo Rodrigues <hugogabriel.es@gmail.com>
2024-08-29 22:03:46 -0300

    Atualizando dependencias

---
+++ locadora/src/main/java/br/edu/solutis/dev/trail/locadora/model/dto/aluguel/carroDto.java
@@ -0,0 +1,12 @@
+package br.edu.solutis.dev.trail.locadora.model.dto.aluguel;
+
+import jakarta.validation.contrains.notBlank;
+import jakarta.validation.contrains.notNull;
+import jakarta.validation.contrains.Size;
+imrpot 
+
+
+
+public class carroDto {
+    
+}
