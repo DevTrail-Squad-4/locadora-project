b82726dab4ac63f050bfb9c5577352af49417217
gabriel21lva <gabrielsilvaesousa@hotmail.com>
2024-08-30 22:29:45 -0300

    Feat: Adicionando Repositorio Fabricante

---
+++ locadora/repository/carro/FabricanteRepositorio.java
@@ -0,0 +1,11 @@
+package br.com.solutis.locadora.repository.carro;
+
+import br.com.solutis.locadora.model.entity.car.Manufacturer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ManufacturerRepository extends JpaRepository<Manufacturer, Long> {
+    Page<Manufacturer> findByDeletedFalse(Pageable pageable);
