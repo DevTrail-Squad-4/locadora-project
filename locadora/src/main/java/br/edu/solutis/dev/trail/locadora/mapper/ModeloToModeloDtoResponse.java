b3ebcf5bb0580c65f88bf5ef7f0727175a7845e3
thiagonox <thiagogentil499@gmail.com>
2024-09-01 11:45:05 -0300

    :bug:  fix: Conflito de Getters e Setters

--- locadora/src/main/java/br/edu/solutis/dev/trail/locadora/mapper/ModeloToModeloDtoResponse.java
+++ locadora/src/main/java/br/edu/solutis/dev/trail/locadora/mapper/ModeloToModeloDtoResponse.java
@@ -12,7 +12,7 @@ public class ModeloToModeloDtoResponse extends AbstractConverter<Modelo, ModeloD
         modeloDtoResponse.setId(modelo.getId());
         modeloDtoResponse.setCategory(modelo.getCategory());
         modeloDtoResponse.setModel(modelo.getDescription());
-        modeloDtoResponse.setFabricante(modelo.getFabricante().getNome());
+        modeloDtoResponse.setManufacturer(modelo.getManufacturer().getName());
 
         return modeloDtoResponse;
     }
