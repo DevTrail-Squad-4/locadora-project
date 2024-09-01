92867d38bd03644bb67cfe2c7241594db2dc97bd
DanielNaiff <danielnaiff344@gmail.com>
2024-08-31 22:32:56 -0300

    fix: names

--- locadora/src/main/java/br/edu/solutis/dev/trail/locadora/exception/pessoa/motorista/MotoristaException.java
+++ locadora/src/main/java/br/edu/solutis/dev/trail/locadora/exception/pessoa/motorista/MotoristaException.java
@@ -3,4 +3,4 @@ package br.edu.solutis.dev.trail.locadora.exception.pessoa.motorista;
 public class MotoristaException extends RuntimeException {
     public MotoristaException(String message, Throwable cause) {
         super(message, cause);
-    }
+    }}
