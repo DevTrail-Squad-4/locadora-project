71f30524ed5a84d9dc4c1c16844e53aa2f8a68a8
thiagonox <thiagogentil499@gmail.com>
2024-08-31 22:16:26 -0300

    :books: Docs: Ajuste Documentação Swagger

--- locadora/src/main/java/br/edu/solutis/dev/trail/locadora/model/dto/carro/FabricanteDto.java
+++ locadora/src/main/java/br/edu/solutis/dev/trail/locadora/model/dto/carro/FabricanteDto.java
@@ -11,8 +11,8 @@ import lombok.EqualsAndHashCode;
 public class FabricanteDto {
     private Long id;
 
-    @NotNull(message = "Name is required")
-    @NotBlank(message = "Name is required")
-    @Size(min = 1, max = 255, message = "Name must be between 1 and 255 characters long")
+    @NotNull(message = "Nome é obrigatório")
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(min = 1, max = 255, message = "Nome deve ter entre 1 e 255 caracteres")
     private String name;
 }
