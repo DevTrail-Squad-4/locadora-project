c1e3ef644f70aaa9c39a88468abc3afa8691ae12
DanielNaiff <danielnaiff344@gmail.com>
2024-09-01 23:29:10 -0300

    fix : name cpf to email

--- locadora/src/main/java/br/edu/solutis/dev/trail/locadora/service/pessoa/MotoristaService.java
+++ locadora/src/main/java/br/edu/solutis/dev/trail/locadora/service/pessoa/MotoristaService.java
@@ -43,7 +43,7 @@ public class MotoristaService implements CrudService<MotoristaDTO> {
         Motorista motorista = driverRepository.findByCpf(cpf)
                 .orElseThrow(() -> new MotoristaCpfNotFoundException(cpf));
 
-        return "O email já existe!";
+        return "O cpf já existe!";
     }
 
     public MotoristaDTO findById(Long id) {
